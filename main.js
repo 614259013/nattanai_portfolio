c91a2c53e4a946a09045cf9911df3040bc4134c3
nattanai <614259013@webmail.npru.ac.th>
2020-10-05 16:49:50 +0700

    initial project

---
+++ main.js
@@ -0,0 +1,25 @@
+const navMenu = document.getElementById('nav-menu');
+const toggleMenu = document.getElementById('nav-toggle');
+const closeMenu = document.getElementById('nav-close');
+
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show')
+})
+closeMenu.addEventListener('click',()=>{
+  navMenu.classList.remove('show')
+})
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sections = document.querySelectorAll('scrollActiove')
+
+function scrollActiove(){
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    
+}
